6b6f92f7facbb8d7f6e46bffa51424c4f40e3806
Eric Burdett <eric@burdett.us>
2020-06-19 11:02:46 -0600

    Updated inference script to conform with ArgumentParser changes

--- inference.py
+++ inference.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 
 from src.lineseg.model import ARUNet
 from src.lineseg.dataset.sequence import ARUSequence
-from src.lineseg.util.arguments import ArgumentParser
+from src.lineseg.util.arguments import IArg, InfArgParser
 from src.lineseg.seg import segment_from_predictions
 
 
-def inference(args):
+def inference(cmd_args):
     """
     Perform inference on images specified by the user
 
@@ -26,20 +26,20 @@ def inference(args):
     * image_resize (optional): The height and width for resizing the image when sent into the model for inference
                                (default: 768, 1152)
 
-    :param args: Command line arguments
+    :param cmd_args: Command line arguments
     :return: None
     """
 
-    a = ArgumentParser(args)
-    arg_dict = a.parse_inference_arguments()
+    args = InfArgParser(cmd_args)
+    args.parse()
 
-    sequence = ARUSequence(arg_dict[a.IMG_PATH])
+    sequence = ARUSequence(args[IArg.IMG_PATH])
     baseline_model = ARUNet()
     seam_model = ARUNet()
 
     # Load the pre-trained model weights
-    baseline_model.load_weights(arg_dict[a.WEIGHTS_PATH_BASELINE])
-    seam_model.load_weights(arg_dict[a.WEIGHTS_PATH_SEAM])
+    baseline_model.load_weights(args[IArg.WEIGHTS_PATH_BASELINE])
+    seam_model.load_weights(args[IArg.WEIGHTS_PATH_SEAM])
 
     # Iterate through each of the images and perform inference
     for img, img_name in tqdm(sequence):
@@ -47,8 +47,8 @@ def inference(args):
         seam_prediction = seam_model(tf.expand_dims(img, 0))
 
         segment_from_predictions(img, baseline_prediction, seam_prediction, img_name,
-                                 int(arg_dict[a.SEGMENTATION_STEP_SIZE]), plot_images=arg_dict[a.SHOULD_PLOT_IMAGES],
-                                 save_path=arg_dict[a.OUT_PATH])
+                                 int(args[IArg.SEGMENTATION_STEP_SIZE]), plot_images=bool(args[IArg.SHOULD_PLOT_IMAGES]),
+                                 save_path=args[IArg.OUT_PATH])
 
     print('Finished performing inference.')
 
